1abf8f18aca79679bb0d3fa7e653007db783aafe
Eddysam2000 <samueledwin67@gmail.com>
2023-10-16 13:03:44 +0100

    navbar adjusted

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,74 +1,29 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './Navbar.css'
-
-const Navbar = () => {
+import {FaStudiovinari} from "react-icons/fa"
+const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="Head">
+    <Container>
+    <Navbar.Brand href="#home" className='fs-2 text-white'>
+      <FaStudiovinari/>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className=" ms-auto">
+        <Nav.Link href="#" className='fw-bold text-white'>Home</Nav.Link>
+        <Nav.Link href="#" className='fw-bold text-white'>Jobs</Nav.Link>
+        <Nav.Link href="#" className='fw-bold text-white'>Login</Nav.Link>
+        <Nav.Link href="#" className='fw-bold text-white'>Log-Out</Nav.Link>
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+  </Navbar>
   );
 }
 
-export default Navbar
+export default Header
