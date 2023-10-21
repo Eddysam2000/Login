fdedecd3fa1e33953ac9a54b9abfaa9e1ae7cec3
Eddysam2000 <samueledwin67@gmail.com>
2023-10-21 23:59:12 +0100

    job page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
+import Jobs from './Components/Jobs/Jobs';
 
 function App() {
   return (
-    <div>
+    <div className=''>
+      <BrowserRouter>
       <Navbar/>
-      <Login/>
+        <Routes>
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Jobs' element={<Jobs />} />
+        </Routes>
+
+      </BrowserRouter>
+      
+      
     </div>
   );
 }
