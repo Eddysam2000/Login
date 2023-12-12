65a5a54cc1052072c23a69a4099a23da11311231
Eddysam2000 <samueledwin67@gmail.com>
2023-12-12 13:14:38 +0100

    Job updated

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   return (
     <div className='container1'>
         <div className='header'>
-            <div className="text">Logins</div>
+            <div className="text">Login</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
@@ -22,7 +22,7 @@ const Login = () => {
             </div>
         </div>
         <div className="submit-container">
-            <div className="submit">Logins</div>
+            <div className="submit">Login</div>
         </div>
 
     </div>
